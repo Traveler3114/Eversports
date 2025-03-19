6a9e9d144715981e6516553dde953e1cd1559708
Traveler3114 <fasterthanblaster@gmail.com>
2025-03-19 13:38:57 +0100

    Update LookingToPlayPage.xaml.cs

--- Eversports/Pages/LookingToPlayPage.xaml.cs
+++ Eversports/Pages/LookingToPlayPage.xaml.cs
@@ -2,6 +2,10 @@ namespace Eversports.Pages;
 
 public partial class LookingToPlayPage : ContentPage
 {
+
+	//stranica gdje korisnici mogu postavljati trazenja igraca
+	//lista na kojoj pise koji korisnik trazi igraca, koji sport i koja lokacija,
+	//takoder taj korisnik moze postaviti odredenjeno vrijeme, na primjer subota ili nedelja izmedju 10-12
 	public LookingToPlayPage()
 	{
 		InitializeComponent();
