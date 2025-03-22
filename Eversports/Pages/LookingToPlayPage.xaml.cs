81cbfbc4b6b93688906781272d3cb26064302fdf
Traveler3114 <fasterthanblaster@gmail.com>
2025-03-22 19:34:32 +0100

    Save

--- Eversports/Pages/LookingToPlayPage.xaml.cs
+++ Eversports/Pages/LookingToPlayPage.xaml.cs
@@ -5,26 +5,30 @@ using CsvHelper;
 using System.Diagnostics.Metrics;
 using System.Globalization;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace Eversports.Pages;
 
 public partial class LookingToPlayPage : ContentPage
 {
-
     //stranica gdje korisnici mogu postavljati trazenja igraca
     //lista na kojoj pise koji korisnik trazi igraca, koji sport i koja lokacija,
     //takoder taj korisnik moze postaviti odredenjeno vrijeme, na primjer subota ili nedelja izmedju 10-12
+    List<string> ChoosenSports {  get; set; }=new List<string>();
 
     public LookingToPlayPage()
 	{
 		InitializeComponent();
         SportPicker.ItemsSource = new List<string>
         {
+            "Running",
+            "Hiking",
+            "Tennis",
+            "Table Tennis",
+            "Cycling",
             "Football",
             "Basketball",
             "Cricket",
-            "Tennis",
-            "Table Tennis",
             "Baseball",
             "Golf",
             "Volleyball",
@@ -32,17 +36,33 @@ public partial class LookingToPlayPage : ContentPage
             "Boxing",
             "Swimming",
             "Athletics",
-            "Cycling",
             "Handball",
             "Gymnastics",
             "Skiing and Snowboarding",
             "Surfing",
-            "Skateboarding"
+            "Skateboarding",
+
         };
     }
 
     private void OnSportSelected(object sender, EventArgs e)
     {
-        var selectedSport = SportPicker.SelectedItem.ToString();
+        if (!(ChoosenSports.Count > 5))
+        {
+            string selectedSport = SportPicker.SelectedItem.ToString()!;
+            if (!ChoosenSports.Contains(selectedSport))
+            {
+                ItemStackLayout.Children.Add(new ItemView(selectedSport));
+                ChoosenSports.Add(selectedSport);
+            }
+            else
+            {
+                DisplayAlert("Error", "You already choose " + selectedSport, "OK");
+            }
+        }
+        else
+        {
+            DisplayAlert("Too many sports", "I think you have already choosen to many sports, don't you? ", "OK");
+        }
     }
 }
--- Eversports/Views/ItemView.xaml.cs
+++ Eversports/Views/ItemView.xaml.cs
@@ -1,9 +1,23 @@
+using System.Collections.ObjectModel;
+
 namespace Eversports.Views;
 
 public partial class ItemView : ContentView
 {
-	public ItemView()
-	{
-		InitializeComponent();
-	}
+
+
+    public ItemView(string text)
+    {
+        InitializeComponent();
+        ItemLabel.Text = text;
+    }
+
+    private void OnRemoveButtonClicked(object sender, EventArgs e)
+    {
+        var parent = this.Parent as HorizontalStackLayout;
+        if (parent != null)
+        {
+            parent.Children.Remove(this);
+        }
+    }
 }
