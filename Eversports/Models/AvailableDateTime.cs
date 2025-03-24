d7d2a76f1f5971fa2577a80ecf44211a9b6f1993
Traveler3114 <fasterthanblaster@gmail.com>
2025-03-24 09:57:24 +0100

    Save

---
+++ Eversports/Models/AvailableDateTime.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eversports.Models
+{
+    class AvailableDateTime
+    {
+        public string Date { get; set; }
+        public string FromTime { get; set; }
+        public string ToTime { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as AvailableDateTime;
+            return Date == other.Date && FromTime == other.FromTime && ToTime == other.ToTime;
+        }
+    }
+}
--- Eversports/Pages/LookingToPlayPage.xaml.cs
+++ Eversports/Pages/LookingToPlayPage.xaml.cs
@@ -14,11 +14,11 @@ public partial class LookingToPlayPage : ContentPage
     //stranica gdje korisnici mogu postavljati trazenja igraca
     //lista na kojoj pise koji korisnik trazi igraca, koji sport i koja lokacija,
     //takoder taj korisnik moze postaviti odredenjeno vrijeme, na primjer subota ili nedelja izmedju 10-12
-    List<string> ChoosenSports {  get; set; }=new List<string>();
-
+    List<string> ChoosenSports { get; set; } = new List<string>();
+    List<AvailableDateTime> availableDateTimes { get; set; }=new List<AvailableDateTime>();
     public LookingToPlayPage()
-	{
-		InitializeComponent();
+    {
+        InitializeComponent();
         SportPicker.ItemsSource = new List<string>
         {
             "Running",
@@ -41,8 +41,8 @@ public partial class LookingToPlayPage : ContentPage
             "Skiing and Snowboarding",
             "Surfing",
             "Skateboarding",
-
         };
+        BindingContext = this;
     }
 
     private void OnSportSelected(object sender, EventArgs e)
@@ -52,7 +52,7 @@ public partial class LookingToPlayPage : ContentPage
             string selectedSport = SportPicker.SelectedItem.ToString()!;
             if (!ChoosenSports.Contains(selectedSport))
             {
-                ItemStackLayout.Children.Add(new ItemView(selectedSport));
+                SportStackLayout.Children.Add(new ItemView(selectedSport));
                 ChoosenSports.Add(selectedSport);
             }
             else
@@ -63,6 +63,27 @@ public partial class LookingToPlayPage : ContentPage
         else
         {
             DisplayAlert("Too many sports", "I think you have already choosen to many sports, don't you? ", "OK");
+        }
+    }
+
+    private void OnAddDateTimeClicked(object sender, EventArgs e)
+    {
+
+        AvailableDateTime dateTime = new AvailableDateTime
+        {
+            Date = DatePicker.Date.ToString("d"),
+            FromTime = FromTimePicker.Time.ToString(@"hh\:mm"),
+            ToTime = FromTimePicker.Time.ToString(@"hh\:mm")
+        };
+        if (!availableDateTimes.Contains(dateTime))
+        {
+            availableDateTimes.Add(dateTime);
+            string dateTimeEntry = $"{dateTime.Date} from {dateTime.FromTime} to {dateTime.ToTime}";
+            TimeStackLayout.Children.Add(new ItemView(dateTimeEntry));
+        }
+        else
+        {
+            DisplayAlert("Error", "You already selected that date and time", "Ok");
         }
     }
 }
