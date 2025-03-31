a38196667226281b53516fddd801614bbb19c29f
Traveler3114 <fasterthanblaster@gmail.com>
2025-03-31 10:03:26 +0200

    Update App.xaml.cs

--- Eversports/App.xaml.cs
+++ Eversports/App.xaml.cs
@@ -11,9 +11,9 @@ namespace Eversports
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-#if DEBUG
-            return new Window(new AppShellMain());
-#endif
+//#if DEBUG
+//            return new Window(new AppShellMain());
+//#endif
 
             // Replace this with your actual login check
             var isUserLoggedIn = SecureStorage.Default.GetAsync("StayLoggedIn").Result;
