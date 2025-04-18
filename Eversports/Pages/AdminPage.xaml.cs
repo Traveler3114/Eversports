13ec856ab766393363f6ce202ea9d0687741076c
Traveler3114 <fasterthanblaster@gmail.com>
2025-04-18 14:24:08 +0200

    Added admin page

---
+++ Eversports/Pages/AdminPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Eversports.Pages;
+
+public partial class AdminPage : ContentPage
+{
+	public AdminPage()
+	{
+		InitializeComponent();
+	}
+}
--- Eversports/Shells/AppShellMain.xaml.cs
+++ Eversports/Shells/AppShellMain.xaml.cs
@@ -1,3 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Eversports.Models;
 using Microsoft.Maui.Controls;
 
 namespace Eversports.Shells;
@@ -9,11 +17,44 @@ public partial class AppShellMain : Shell
         InitializeComponent();
     }
 
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await CheckRole();
+    }
+
     private void OnLogoutClicked(object sender, EventArgs e)
     {
         SecureStorage.Default.Remove("JWTToken");
         SecureStorage.Default.Remove("StayLoggedIn");
         // Call the OnLogout method in the App class
         ((App)Application.Current!)?.SetToAppShellLogin();
+    }
+
+
+    public async Task CheckRole()
+    {
+        try
+        {
+            var sendingData = new
+            {
+                action = "verifyToken",
+                jwt = await SecureStorage.Default.GetAsync("JWTToken"),
+            };
+            var client = new HttpClient();
+            var jsonContent = JsonSerializer.Serialize(sendingData);
+            var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("http://traveler3114.ddns.net/EversportsAPI/", content);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var deserializedResponse = JsonSerializer.Deserialize<Dictionary<string, string>>(responseContent);
+            if (deserializedResponse["role"] == "user")
+            {
+                AdminFlyoutItem.IsVisible = false; // Hide the Admin FlyoutItem
+            }
+        }
+        catch(Exception ex)
+        {
+            await DisplayAlert("OK", ex.Message, "OK");
+        }
     }
 }
